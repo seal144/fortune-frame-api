0b49f1ccf2213d2bbb1405ae93b7684274baac6a
RafalFak <rafalfak8@gmail.com>
2025-06-11 20:58:21 +0200

    db init

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+from sqlalchemy.engine.url import URL
+
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -10,6 +12,16 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
+    url_object = URL.create(
+        "postgresql+psycopg2",
+        username=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+    )
+
+    SQLALCHEMY_DATABASE_URI = url_object
 
 
 class ProductionConfig(Config):
--- core/__init__.py
+++ core/__init__.py
@@ -2,8 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
+
+database = SQLAlchemy()
 
 
 def create_app(config_type=os.getenv("CONFIG_TYPE")):
@@ -11,4 +14,10 @@ def create_app(config_type=os.getenv("CONFIG_TYPE")):
 
     app.config.from_object(config_type)
 
+    initialize_extensions(app)
+
     return app
+
+
+def initialize_extensions(app):
+    database.init_app(app)
